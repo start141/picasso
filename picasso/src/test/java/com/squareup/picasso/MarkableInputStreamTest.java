ab41565e2db19994a0d1a2ee9cbe92e9d05c7082
start141 <415043846@qq.com>
2015-05-28 17:20:24 +0800

    Update MarkableInputStreamTest.java
    add exceedLimitTest method

--- picasso/src/test/java/com/squareup/picasso/MarkableInputStreamTest.java
+++ picasso/src/test/java/com/squareup/picasso/MarkableInputStreamTest.java
@@ -51,6 +51,30 @@ public class MarkableInputStreamTest {
     } catch (IOException expected) {
     }
   }
+  
+  @Test
+  public void exceedLimitTest() throws Exception {
+    MarkableInputStream in = new MarkableInputStream(new ByteArrayInputStream(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes(Charset.forName("US-ASCII"))));
+    in.allowMarksToExpire(false);
+    assertThat(readBytes(in, 3)).isEqualTo("ABC");
+    long posA = in.savePosition(7);// DEFGHIJ
+    assertThat(readBytes(in, 4)).isEqualTo("DEFG");
+    in.mark(5); // HIJKL
+    assertThat(readBytes(in, 4)).isEqualTo("HIJK");
+    in.reset(); // Back to 'H'
+    assertThat(readBytes(in, 3)).isEqualTo("HIJ");
+    in.reset(posA); // Back to 'D'
+    assertThat(readBytes(in, 7)).isEqualTo("DEFGHIJ");
+    in.reset(); // Back to 'H' again.
+    assertThat(readBytes(in, 6)).isEqualTo("HIJKLM");
+ 
+    in.reset(); // Back to 'H' again despite the original limit being exceeded
+    assertThat(readBytes(in, 2)).isEqualTo("HI"); // confirm we're really back at H
+
+    in.reset(posA); // Back to 'D' again despite the original limit being exceeded
+    assertThat(readBytes(in, 2)).isEqualTo("DE"); // confirm that we're really back at D  
+  }
 
   private String readBytes(InputStream in, int count) throws IOException {
     byte[] result = new byte[count];
